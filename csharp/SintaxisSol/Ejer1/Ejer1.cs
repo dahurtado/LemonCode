7026b44be65b86799ce01358157a826da6791406
dahurtado <dhurtadoperera@gmail.com>
2023-03-24 11:38:02 +0100

    Ejercicios Sintaxis hechos

--- csharp/SintaxisSol/Ejer1/Ejer1.cs
+++ csharp/SintaxisSol/Ejer1/Ejer1.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Sintaxis
+{
+    internal class Ejer1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cual es tu nombre?");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("\nCual es tu edad?");
+            var age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("\nDe donde eres?");
+            var city = Console.ReadLine();
+
+            Console.WriteLine("\nTe llamas " + name + ", tienes " + age + ". Bienvenido a " + city);
+        }
+    }
+}
