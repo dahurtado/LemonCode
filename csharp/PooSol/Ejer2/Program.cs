9693e03c04d4158f7f3544af211ea5fda7c7aeae
dahurtado <dhurtadoperera@gmail.com>
2023-03-27 16:34:53 +0200

    Arreglo en Slot machine

--- csharp/PooSol/Ejer2/Program.cs
+++ csharp/PooSol/Ejer2/Program.cs
@@ -15,47 +15,65 @@ namespace Sintaxis
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\n\t\t --- Bienvenido a las tagraperras! ---\n");
-            int menu = 1;
-            Tragaperra maquina = new Tragaperra();
-            while (menu != 0)
+            int menu_de_juego = 1;
+            var menu_de_programa = "1";
+            while (true)
             {
                 try
                 {
-                    Console.WriteLine("1 - Jugar\n0 - Salir");
-                    menu = Convert.ToInt32(Console.ReadLine());
-                    if (menu < 0 || menu > 1)
+                    Console.WriteLine("\n\t\t --- Bienvenido a las tagraperras! ---\n");
+                    Tragaperra maquina = new Tragaperra();
+                    while (menu_de_juego != 0)
                     {
-                        throw new NumeroFueraDeMenuException("\nERROR - Opcion no disponible en el menu\n");
+                        Console.WriteLine("1 - Jugar\n0 - Salir");
+                        menu_de_juego = Convert.ToInt32(Console.ReadLine());
+                        if (menu_de_juego < 0 || menu_de_juego > 1)
+                        {
+                            throw new NumeroFueraDeMenuException("\nERROR - Opcion no disponible en el menu\n");
+                        }
+                        else
+                        {
+                            switch (menu_de_juego)
+                            {
+                                case 1:
+                                    if (maquina.Play() == true)
+                                    {
+                                        Console.WriteLine("\nFelicidades!! Has ganado " + maquina.Monedas + " monedas!\n");
+                                        menu_de_juego = 0;
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("\nAy amigo casi, siga intentandolo!!\n");
+                                    }
+                                    break;
+                                case 0:
+                                    Console.WriteLine("\nEs una pena que te vayas!\nVuelve pronto!!\n");
+                                    menu_de_programa = "0";
+                                    menu_de_juego = 0;
+                                    break;
+                            }
+                        }
+                    }
+                    if (menu_de_programa == "1")
+                    {
+                        Console.WriteLine("Quieres volver a intentarlo?\nCualquier tecla - Continuar\n0 - Salir");
+                        menu_de_programa = Console.ReadLine();
+                        menu_de_juego = 1;
                     }
                     else
                     {
-                        switch (menu)
-                        {
-                            case 1:
-                                if (maquina.Play() == true)
-                                {
-                                    Console.WriteLine("\nFelicidades!! Has ganado " + maquina.Monedas + " monedas!\n");
-                                    menu = 0;
-                                }
-                                else
-                                {
-                                    Console.WriteLine("\nAy amigo casi, siga intentandolo!!\n");
-                                }
-                                break;
-                            case 0:
-                                break;
-                        }
+                        break;
                     }
                 }
                 catch (FormatException)
+
                 {
                     Console.WriteLine("\nERROR - Numero no introducido\n");
-                    menu = 1;
+                    menu_de_juego = 1;
                 }
                 catch (NumeroFueraDeMenuException)
                 {
-                    menu = 1;
+                    menu_de_juego = 1;
                 }
             }
         }
