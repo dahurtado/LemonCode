8d0c0c094722f9005d3e854a7d47a70d5f39a9f8
dahurtado <dhurtadoperera@gmail.com>
2023-04-25 13:50:30 +0200

    http put hecho

--- angularjs/00-boilerplate/src/app/pages/pelicula-list/edit/pelicula-list.edit.component.ts
+++ angularjs/00-boilerplate/src/app/pages/pelicula-list/edit/pelicula-list.edit.component.ts
@@ -3,28 +3,57 @@ import { PeliculaApiService } from "../pelicula.service";
 
 var baseUrl = window.location.href.split("/");
 var lenghtUrl = baseUrl.length;
+var idUrl = +baseUrl[lenghtUrl - 1]
 
 export class PeliculaEditController {
-	peliculaId: Pelicula = null;
+	pelicula: Pelicula = null;
+	poster: string;
+	name: string;
+	year: number;
+	director: string;
+
 	constructor (private peliculaApiService: PeliculaApiService){
 		"ngInject";
-		this.peliculaId = null;
+		this.pelicula = null;
 	}
 	$onInit() {
-		this.peliculaApiService.getPeliculaId(+baseUrl[lenghtUrl - 1]).then(
-			(result) => {
-				this.peliculaId = result;
-				console.log(this.peliculaId);
-				console.log(result);
-			}
-		)
+		if (baseUrl[lenghtUrl - 1] != "peliculacreate")
+		{
+			this.peliculaApiService.getPeliculaId(idUrl).then(
+				(result) => {
+					this.pelicula = result;
+					console.log(this.pelicula.name);
+					console.log(result);
+				}
+			)
+		}
+	}
+	handleClick = (poster: string, name: string, year: number, director: string) => {
+		this.pelicula.poster = poster;
+		this.pelicula.name = name;
+		this.pelicula.year = year;
+		this.pelicula.director = director;
+
+		if (baseUrl[lenghtUrl - 1] != "peliculacreate")
+		{
+			this.peliculaApiService.updatePelicula(this.pelicula).then(
+				(result) => {
+					console.log(result);
+				}
+			)
+		}
+		else
+		{
+			this.peliculaApiService.insertPelicula(this.pelicula).then(
+				(result) => {
+					console.log(result);
+				}
+			)
+		}
 	}
 }
 
 export const PeliculaListEditComponent = {
-	bindings: {
-		poster: "<"
-	},
 	template: require("./pelicula-list.edit.component.html") as string,
 	controller: PeliculaEditController,
 	controllerAs: "vm",
--- angularjs/00-boilerplate/src/app/pages/pelicula-list/pelicula.service.ts
+++ angularjs/00-boilerplate/src/app/pages/pelicula-list/pelicula.service.ts
@@ -21,7 +21,45 @@ export class PeliculaApiService {
 		return deferred.promise;
 	}
 
+	public updatePelicula(peli: Pelicula): angular.IPromise<Pelicula> {
+		const deferred = this.$q.defer<Pelicula>();
+		let idPeli = peli.id;
+
+		this.$http.put('http://localhost:3001/movies/' + idPeli, peli).then(
+			(result) => {
+				const peliculaInsert = result.data as Pelicula;
+				deferred.resolve(peliculaInsert);
+			}
+		)
+		return deferred.promise;
+	}
+
+	public insertPelicula(peli: Pelicula): angular.IPromise<Pelicula> {
+		const deferred = this.$q.defer<Pelicula>();
+
+		this.$http.post('http://localhost:3001/movies/', peli).then(
+			(result) => {
+				const peliculaInsert = result.data as Pelicula;
+				deferred.resolve(peliculaInsert);
+			}
+		)
+		return deferred.promise;
+	}
+
 	public getPeliculaId(id: number): angular.IPromise<Pelicula> {
+		const deferred = this.$q.defer<Pelicula>();
+
+		this.$http.get('http://localhost:3001/movies/' + id).then(
+			(result) => {
+				const peliculas = result.data as Pelicula;
+				deferred.resolve(peliculas);
+			}
+		);
+
+		return deferred.promise;
+	}
+
+	public deletePelicula(id: number): angular.IPromise<Pelicula> {
 		const deferred = this.$q.defer<Pelicula>();
 
 		this.$http.get('http://localhost:3001/movies/' + id).then(
