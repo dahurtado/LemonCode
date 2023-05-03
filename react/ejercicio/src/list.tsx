4d40a641d2477f9fd30acda20866d7f3da9b651f
dahurtado <dhurtadoperera@gmail.com>
2023-05-03 17:26:35 +0200

    ejer 1 casi listo

--- react/ejercicio/src/list.tsx
+++ react/ejercicio/src/list.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useDebounce } from 'use-debounce';
 
 interface MemberEntity {
   id: string;
@@ -12,7 +11,6 @@ interface MemberEntity {
 export const ListPage: React.FC = () => {
   const [members, setMembers] = React.useState<MemberEntity[]>([]);
   const [org, setOrg] = React.useState("lemoncode");
-  const [debounceOrg] = useDebounce(org, 1000);
 
   console.log(org);
   let orgUrl = "https://api.github.com/orgs/" + org + "/members";
@@ -23,33 +21,60 @@ export const ListPage: React.FC = () => {
     fetch(orgUrl)
       .then((response) => response.json())
       .then((json) => setMembers(json));
-  }, [debounceOrg]);
+  }, [org]);
 
-  return (
-    <>
-      <h2>Hello from List page</h2>+{" "}
+  if (members.map == null) {
+    return (
+      <>
+        <h2>Hello from List page</h2>+{" "}
+  
+        <div>
+          <input 
+            type="text"
+            value={org}
+            onChange={(e) => setOrg(e.target.value)}
+            />
+        </div>
+  
+        <div className="list-user-list-container">
+          <span className="list-header">Avatar</span>
+          <span className="list-header">Id</span>
+          <span className="list-header">Name</span>
+          <p>Organizacion no encontrada</p>
+        </div>
+      </>
+    );
+  }
+  else
+  {
+    return (
+      <>
+        <h2>Hello from List page</h2>+{" "}
 
-      <div>
-        <input 
-          type="text"
-          value={org}
-          onChange={(e) => setOrg(e.target.value)}
-        />
-      </div>
+        <div>
+          <input 
+            type="text"
+            value={org}
+            onChange={(e) => setOrg(e.target.value)}
+          />
+        </div>
+  
+        <div className="list-user-list-container">
+          <span className="list-header">Avatar</span>
+          <span className="list-header">Id</span>
+          <span className="list-header">Name</span>
+          {members.map((member) => (
+            <>
+              <img src={member.avatar_url} />
+              <span>{member.id}</span>
+              <Link to={`/detail/${member.login}`}>{member.login}</Link>
+            </>
+          ))}
+        </div>
+        <Link to="/detail">Navigate to detail page</Link>
+      </>
+    );
+  }
 
-      <div className="list-user-list-container">
-        <span className="list-header">Avatar</span>
-        <span className="list-header">Id</span>
-        <span className="list-header">Name</span>
-        {members.map((member) => (
-          <>
-            <img src={member.avatar_url} />
-            <span>{member.id}</span>
-            <Link to={`/detail/${member.login}`}>{member.login}</Link>
-          </>
-        ))}
-      </div>
-      <Link to="/detail">Navigate to detail page</Link>
-    </>
-  );
+  
 };
